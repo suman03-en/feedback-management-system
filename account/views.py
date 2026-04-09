383621ba9633b61c31a5f499c2c8ca7852c30567
suman phuyal <sfuyal59@gmail.com>
2026-04-09 14:07:54 +0545

    Add useregister view with template

--- account/views.py
+++ account/views.py
@@ -1,8 +1,21 @@
 from django.views import View
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, HttpResponse
+from .forms import UserRegistrationForm
+
 
 class UserRegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = "account/register.html"
+
+    def get_form(self, *args, **kwargs):
+        return self.form_class(*args, **kwargs)
+
     def get(self, request):
-        return render(request, "account/register.html", {"form": UserCreationForm()})
-    
+        return render(request, self.template_name, {"form": self.get_form()})
+
+    def post(self, request):
+        form = self.get_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("User registered successfully")
+        return render(request, self.template_name, {"form": form})
